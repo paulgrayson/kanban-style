2179a15e6014652836da920ecb8bc92d894f0a84
Paul Grayson <paulgrayson@users.noreply.github.com>
2014-07-26 15:41:09 +0100

    Rework signin and projects page using bootstrap

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
 
-  def field_with_errors(model, field_name, &block)
+  def field_with_errors(model, field_name, css_classes='', &block)
     has_error = model.errors[field_name].present?
-    content_tag(:div, id: "user_#{field_name}_field", class: 'field') do
-      concat content_tag(:div, @user.errors[:email].join(', '), class: 'error') if has_error
+    content_tag(:div, id: "user_#{field_name}_field", class: "#{css_classes}#{has_error ? ' has-error' : ''}") do
       concat capture(&block) if block_given?
+      concat content_tag(:span, @user.errors[field_name].join(', '), class: 'help-block') if has_error
     end
   end
 
